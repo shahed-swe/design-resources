b9a4cf8ae99c0a3f347e2416d58f8a8c4b4ed300
Md Shahed Talukder <shahedtalukder51@gmail.com>
2022-05-28 19:08:38 +0600

    making of useScript component done

--- customhooks/src/App.js
+++ customhooks/src/App.js
@@ -8,19 +8,34 @@ import PreviousComponent from "./hooks/usePrevious/examples/index";
 import StateWithHistoryComponent from "./hooks/useStateWithHistory/examples";
 import StorageComponent from "./hooks/useStorage/examples";
 import AsyncComponent from "./hooks/useAsync/examples";
+import FetchComponent from "./hooks/useFetch/examples";
+import ScriptComponent from "./hooks/useScript/examples";
 
 function App() {
   return (
     <div className="App">
+      <h1>debounce</h1>
       <DebounceComponent/>
+      <h1>time out</h1>
       <TimeOutComponent/>
+      <h1>toggle</h1>
       <ToggleComponent/>
+      <h1>update effect</h1>
       <UpdateEffectComponent/>
+      <h1>array component</h1>
       <ArrayComponent/>
+      <h1>previous component</h1>
       <PreviousComponent/>
+      <h1>state with history</h1>
       <StateWithHistoryComponent/>
+      <h1>storage component</h1>
       <StorageComponent/>
+      <h1>async component</h1>
       <AsyncComponent/>
+      <h1>fetch component</h1>
+      <FetchComponent/>
+      <h1>script component</h1>
+      <ScriptComponent/>
     </div>
   );
 }
